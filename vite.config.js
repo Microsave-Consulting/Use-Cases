8f187a917476ea3ec0810010c409a91a8f3c04b4
Suhird Singh <suhird.singh@microsave.net>
2025-12-09 11:13:58 +0530

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
+
 export default defineConfig({
   base: "/Use-Cases/",   // Must match your repo name EXACTLY
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Use-Cases/',   // MUST match your GitHub repo name
+
   plugins: [react()],
 });
